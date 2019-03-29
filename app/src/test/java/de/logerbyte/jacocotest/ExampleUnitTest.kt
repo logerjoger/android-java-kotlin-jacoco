5ffcb5f25721634d687c9f2286b397aff639d29a
Stefan Wolfram <Lizenzen-IT-Berlin@thalia.de>
2019-03-29 14:49:03 +0100

    init commit

---
+++ app/src/test/java/de/logerbyte/jacocotest/ExampleUnitTest.kt
@@ -0,0 +1,19 @@
+package de.logerbyte.jacocotest
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
