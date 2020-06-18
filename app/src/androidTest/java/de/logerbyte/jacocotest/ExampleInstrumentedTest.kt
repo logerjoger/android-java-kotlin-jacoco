cc86c980c48f5da90d4c9d6f59159052336af0a3
Loger Joger <logerom@mailbox.org>
2020-06-18 16:30:33 +0200

    espresso: setup + add simple test

--- app/src/androidTest/java/de/logerbyte/jacocotest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/de/logerbyte/jacocotest/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package de.logerbyte.jacocotest
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+
 
 import org.junit.Test
-import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,19 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var activityScenarioRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("de.logerbyte.jacocotest", appContext.packageName)
+    }
+
+    @Test
+    fun initMainActivity() {
+        Espresso.onView(ViewMatchers.withText("Hello World!")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
 }
